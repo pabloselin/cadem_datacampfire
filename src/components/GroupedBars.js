c3517e50f87606d68fe6f5186409be9b6e2f8ff9
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2018-04-13 01:54:09 -0300

    funcionalidad barras agrupadas

--- src/components/GroupedBars.js
+++ src/components/GroupedBars.js
@@ -8,6 +8,7 @@ import {
 	VictoryLabel,
 	VictoryAxis,
 	VictoryContainer,
+	VictorySharedEvents,
 	Point
 } from "victory";
 
@@ -19,9 +20,13 @@ class GroupedBars extends Component {
 			subtitle: this.props.data.chart_subtitle,
 			data: this.props.data,
 			activeKey: null,
+			activeCat: null,
 			activeColor: this.props.theme.interactions.hover,
+			colorscale: this.props.theme.line.colorScale,
 			clicked: false,
-			svgrefs: []
+			svgrefs: [],
+			domainY: [0, 100],
+			domainX: [0, 100]
 		};
 	}
 
@@ -45,6 +50,22 @@ class GroupedBars extends Component {
 		return legData;
 	}
 
+	getCurFill(cat, index, active) {
+		if (this.state.activeCat === cat || active === true) {
+			return this.state.activeColor;
+		} else {
+			return this.state.colorscale[index];
+		}
+	}
+
+	getLabelState(cat, index, active) {
+		if (this.state.activeCat === cat || active === true) {
+			return this.state.activeColor;
+		} else {
+			return "transparent";
+		}
+	}
+
 	componentDidMount() {
 		this.setState({
 			svgrefs: [this.containerRef]
@@ -52,20 +73,202 @@ class GroupedBars extends Component {
 	}
 
 	render() {
+		const activeStyle = [
+			{
+				target: "data",
+				mutation: props => ({
+					style: Object.assign({}, props.style, {
+						fill: this.state.activeColor
+					})
+				})
+			},
+			{
+				target: "labels",
+				mutation: props => ({
+					style: Object.assign({}, props.style, {
+						display: "block",
+						fill: this.state.activeColor,
+						fontSize: 8,
+						fontWeight: "bold"
+					})
+				})
+			}
+		];
+		const normalStyle = [
+			{
+				target: "data",
+				mutation: props => null
+			},
+			{
+				target: "labels",
+				mutation: props => null
+			}
+		];
+
+		const labelStyle = {
+			fontWeight: a => {
+				if (
+					this.state.clickedKeys.indexOf(a) !== -1 ||
+					this.state.activeKey === a
+				) {
+					return "bold";
+				} else {
+					return "normal";
+				}
+			},
+			fill: a => {
+				if (
+					this.state.clickedKeys.indexOf(a) !== -1 ||
+					this.state.activeKey === a
+				) {
+					return this.state.activeColor;
+				} else {
+					return "#555";
+				}
+			}
+		};
+
+		const legendLabelStyle = {
+			fontSize: 8,
+			fontFamily: "Asap",
+			fontWeight: a => {
+				if (this.state.activeCat === a.name) {
+					return "bold";
+				} else {
+					return "normal";
+				}
+			},
+			fill: a => {
+				if (this.state.activeCat === a.name) {
+					return this.state.activeColor;
+				} else {
+					return "#555";
+				}
+			}
+		};
+
+		const legendDataStyle = {
+			fill: a => {
+				if (this.state.activeCat === a.name) {
+					return this.state.activeColor;
+				} else {
+					return a.symbol.fill;
+				}
+			}
+		};
+
+		const events = [
+			{
+				childName: "all",
+				target: "data",
+				eventHandlers: {
+					onMouseOver: (evt, obj, idx) => {
+						if (this.state.clicked !== true) {
+							let activeCat = obj.data[0].cat;
+							this.setState({
+								activeKey: activeCat
+							});
+							return activeStyle;
+						}
+					},
+					onClick: (evt, obj, idx) => {
+						let activeCat = obj.data[0].cat;
+						this.setState({
+							activeKey: activeCat,
+							clicked: true
+						});
+						return activeStyle;
+					},
+					onMouseOut: () => {
+						if (this.state.clicked !== true) {
+							this.setState({ activeKey: null });
+
+							return normalStyle;
+						}
+					}
+				}
+			},
+			{
+				childName: "legend",
+				target: "data",
+				eventHandlers: {
+					onClick: (evt, obj, key) => {
+						if (obj.datum !== undefined) {
+							let refName = obj.datum.name;
+							if (this.state.clicked !== true) {
+								this.setState({
+									activeCat: refName,
+									clicked: true
+								});
+							} else {
+								//Está cliqueada una leyenda
+								if (this.state.activeCat === refName) {
+									this.setState({
+										activeCat: null,
+										clicked: false
+									});
+								} else {
+									this.setState({ activeCat: refName });
+								}
+							}
+						}
+					},
+					onMouseOver: (evt, obj, key) => {
+						if (this.state.clicked !== true) {
+							if (obj.datum !== undefined) {
+								this.setState({
+									activeCat: obj.datum.name
+								});
+							}
+						}
+					},
+					onMouseOut: (evt, obj, key) => {
+						if (this.state.clicked !== true) {
+							this.setState({ activeCat: null });
+							return [
+								{
+									target: "data",
+									mutation: props => null
+								},
+								{
+									target: "labels",
+									mutation: props => null
+								}
+							];
+						}
+					}
+				}
+			}
+		];
+
 		const groups = () =>
 			this.state.data.data.map((group, idx) => {
 				return (
 					<VictoryBar
+						name={group.title}
+						theme={this.props.theme}
 						key={"bar-" + idx}
 						colorscale={this.props.theme.colorscale}
 						title={group.title}
 						data={group.data}
+						style={{
+							data: {
+								width: 10,
+								fill: (d, active) =>
+									this.getCurFill(group.title, idx, active)
+							}
+						}}
 						labelComponent={
 							<VictoryLabel
 								style={{
-									display: "none",
-									fontSize: 6,
-									fill: "transparent"
+									fontSize: 8,
+									fontWeight: "bold",
+									fill: (d, active) =>
+										this.getLabelState(
+											group.title,
+											idx,
+											active
+										)
 								}}
 								text={d => `${d.y}%`}
 							/>
@@ -76,162 +279,58 @@ class GroupedBars extends Component {
 
 		return (
 			<div className="chart-widget">
-				<VictoryChart
-					theme={this.props.theme}
-					height={this.props.height}
-					domain={{ y: [0, 100] }}
-					domainPadding={{ x: 40, y: 0 }}
-					containerComponent={
-						<VictoryContainer
-							containerRef={containerRef =>
-								(this.containerRef = containerRef)
-							}
-						/>
-					}
-				>
-					<VictoryLegend
-						title={[
-							this.state.title.toUpperCase(),
-							this.state.subtitle
-						]}
-						width={this.props.width}
-						titleOrientation="left"
+				<VictorySharedEvents events={events}>
+					<VictoryChart
 						theme={this.props.theme}
-						name="legend"
-						data={this.makeLegend(this.state.data)}
-						orientation="vertical"
-						itemsPerRow={1}
-						height={60}
-						style={{
-							title: { fontSize: 12, fontWeight: "bold" },
-							labels: { fontSize: 8 }
-						}}
-						labelComponent={<VictoryLabel y={12} />}
-						dataComponent={<Point y={12} />}
-						titleComponent={
-							<VictoryLabel
-								style={[
-									{ fontSize: 10 },
-									{ fontSize: 6, fontWeight: "normal" }
-								]}
+						height={this.props.height}
+						domain={{ y: [0, 100] }}
+						domainPadding={{ x: 40, y: 0 }}
+						containerComponent={
+							<VictoryContainer
+								containerRef={containerRef =>
+									(this.containerRef = containerRef)
+								}
 							/>
 						}
-					/>
-					<VictoryAxis
-						theme={this.props.theme}
-						style={{
-							tickLabels: {
-								fontSize: 6
-							}
-						}}
-					/>
-					<VictoryAxis
-						dependentAxis
-						theme={this.props.theme}
-						style={{
-							tickLabels: {
-								fontSize: 6
-							}
-						}}
-					/>
-					<VictoryGroup
-						name="BarGroup"
-						categories={{ x: this.state.data.categories }}
-						offset={14}
-						events={[
-							{
-								childName: "all",
-								target: "data",
-								eventHandlers: {
-									onMouseOver: (evt, obj, idx) => {
-										this.setState({
-											activeKey: obj.data[0].label
-										});
-										return [
-											{
-												target: "data",
-												mutation: props => ({
-													style: Object.assign(
-														{},
-														props.style,
-														{
-															fill: this.state
-																.activeColor
-														}
-													)
-												})
-											},
-											{
-												target: "labels",
-												mutation: props => ({
-													style: Object.assign(
-														{},
-														props.style,
-														{
-															fontSize: 6,
-															fill: this.state
-																.activeColor
-														}
-													)
-												})
-											}
-										];
-									},
-									onClick: (evt, obj, idx) => {
-										this.setState({
-											activeKey: obj.data[0].label,
-											clicked: true
-										});
-										return [
-											{
-												target: "data",
-												mutation: props => ({
-													style: Object.assign(
-														{},
-														props.style,
-														{
-															fill: this.state
-																.activeColor
-														}
-													)
-												})
-											},
-											{
-												target: "labels",
-												mutation: props => ({
-													style: Object.assign(
-														{},
-														props.style,
-														{
-															display: "block",
-															fill: this.state
-																.activeColor
-														}
-													)
-												})
-											}
-										];
-									},
-									onMouseOut: () => {
-										this.setState({ activeKey: null });
-										return [
-											{
-												target: "data",
-												mutation: props => null
-											},
-											{
-												target: "labels",
-												mutation: props => null
-											}
-										];
-									}
-								}
-							}
-						]}
 					>
-						{groups()}
-					</VictoryGroup>
-				</VictoryChart>
+						<VictoryLegend
+							title={[
+								this.state.title.toUpperCase(),
+								this.state.subtitle
+							]}
+							width={this.props.width}
+							titleOrientation="left"
+							theme={this.props.theme}
+							name="legend"
+							data={this.makeLegend(this.state.data)}
+							orientation="vertical"
+							itemsPerRow={1}
+							height={60}
+							labelComponent={
+								<VictoryLabel style={legendLabelStyle} y={12} />
+							}
+							dataComponent={
+								<Point style={legendDataStyle} y={12} />
+							}
+							titleComponent={
+								<VictoryLabel
+									style={[
+										{ fontSize: 10 },
+										{ fontSize: 6, fontWeight: "normal" }
+									]}
+								/>
+							}
+						/>
+
+						<VictoryGroup
+							name="BarGroup"
+							categories={{ x: this.state.data.categories }}
+							offset={14}
+						>
+							{groups()}
+						</VictoryGroup>
+					</VictoryChart>
+				</VictorySharedEvents>
 				<DownloadButton
 					type="groupedbars"
 					svgs={this.state.svgrefs}
