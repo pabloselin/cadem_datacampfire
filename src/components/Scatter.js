6f9c5e94abe271a631adfc3fed2d67afb9cb966d
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2018-04-13 00:53:41 -0300

    semi funcionalidad scatter

--- src/components/Scatter.js
+++ src/components/Scatter.js
@@ -25,7 +25,9 @@ class Scatter extends Component {
 			activeCat: null,
 			domainX: this.props.data.domainX,
 			domainY: this.props.data.domainY,
-			svgrefs: []
+			svgrefs: [],
+			clicked: false,
+			clickedKeys: []
 		};
 	}
 
@@ -50,6 +52,31 @@ class Scatter extends Component {
 			return this.state.activeColor;
 		} else {
 			return this.state.colorscale[index];
+		}
+	}
+
+	removeKey(array, element) {
+		return array.filter(e => e !== element);
+	}
+
+	updateClickeds(array, element) {
+		//Debe ser un listado de valores únicos
+		array.push(element);
+		return array.filter((v, i, a) => a.indexOf(v) === i);
+	}
+
+	checkLength(array) {
+		if (array.length == 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	legendLabelStyle() {}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (this.state.activeCat !== prevState.activeCat) {
 		}
 	}
 
@@ -88,97 +115,139 @@ class Scatter extends Component {
 				);
 			});
 
-		return (
-			<div className="chart-widget">
-				<VictorySharedEvents
-					events={[
-						{
-							childName: "all",
-							eventHandlers: {
-								onClick: (evt, obj, key) => {
-									return [
-										{
-											target: "data",
-											mutation: props => {
-												return Object.assign(
-													{},
-													props.style,
-													{
-														style: {
-															fill: this.state
-																.activeColor
-														}
-													}
-												);
-											}
-										}
-									];
+		const legendLabelStyle = {
+			fontSize: 12,
+			fontFamily: "Asap",
+			fontWeight: a => {
+				if (this.state.activeCat === a.name) {
+					return "bold";
+				} else {
+					return "normal";
+				}
+			},
+			fill: a => {
+				if (this.state.activeCat === a.name) {
+					return this.state.activeColor;
+				} else {
+					return "#555";
+				}
+			}
+		};
+
+		const legendDataStyle = {
+			fill: a => {
+				if (this.state.activeCat === a.name) {
+					return this.state.activeColor;
+				} else {
+					return a.symbol.fill;
+				}
+			}
+		};
+
+		const events = [
+			{
+				childName: "all",
+				eventHandlers: {
+					onClick: (evt, obj, key) => {
+						return [
+							{
+								target: "data",
+								mutation: props => {
+									return props.style.fill ===
+										this.state.activeColor
+										? null
+										: {
+												style: {
+													fill: this.state.activeColor
+												}
+										  };
 								}
 							}
-						},
-						{
-							childName: "legend",
-							eventHandlers: {
-								onMouseOver: (evt, obj, key) => {
-									if (obj.datum !== undefined) {
-										this.setState({
-											activeCat: obj.datum.name
-										});
-									}
-
-									return [
-										{
-											target: "data",
-											mutation: props => {
-												return Object.assign(
-													{},
-													props.style,
-													{
-														style: {
-															fill: this.state
-																.activeColor
-														}
-													}
-												);
-											}
-										},
-										{
-											target: "labels",
-											mutation: props => {
-												return Object.assign(
-													{},
-													props.style,
-													{
-														style: {
-															fill: this.state
-																.activeColor,
-															fontWeight: "bold",
-															fontSize: 12,
-															fontFamily: "Asap"
-														}
-													}
-												);
-											}
-										}
-									];
-								},
-								onMouseOut: (evt, obj, key) => {
-									this.setState({ activeCat: null });
-									return [
-										{
-											target: "data",
-											mutation: props => null
-										},
-										{
-											target: "labels",
-											mutation: props => null
-										}
-									];
+						];
+					}
+				}
+			},
+			{
+				childName: "legend",
+				eventHandlers: {
+					onClick: (evt, obj, key) => {
+						if (obj.datum !== undefined) {
+							let refName = obj.datum.name;
+							if (this.state.clicked !== true) {
+								this.setState({
+									activeCat: refName,
+									clicked: true
+								});
+							} else {
+								//Está cliqueada una leyenda
+								if (this.state.activeCat === refName) {
+									this.setState({
+										activeCat: null,
+										clicked: false
+									});
+								} else {
+									this.setState({ activeCat: refName });
 								}
 							}
 						}
-					]}
-				>
+					},
+					onMouseOver: (evt, obj, key) => {
+						if (this.state.clicked !== true) {
+							if (obj.datum !== undefined) {
+								this.setState({
+									activeCat: obj.datum.name
+								});
+							}
+
+							return [
+								{
+									target: "data",
+									mutation: props => {
+										return Object.assign({}, props.style, {
+											style: {
+												fill: this.state.activeColor
+											}
+										});
+									}
+								},
+								{
+									target: "labels",
+									mutation: props => {
+										return Object.assign({}, props.style, {
+											style: {
+												fill: this.state.activeColor,
+												fontWeight: "bold",
+												fontSize: 12,
+												fontFamily: "Asap"
+											}
+										});
+									}
+								}
+							];
+						}
+					},
+					onMouseOut: (evt, obj, key) => {
+						if (this.state.clicked !== true) {
+							this.setState({ activeCat: null });
+							return [
+								{
+									target: "data",
+									mutation: props => null
+								},
+								{
+									target: "labels",
+									mutation: props => null
+								}
+							];
+						}
+					}
+				}
+			}
+		];
+
+		return (
+			<div className="chart-widget">
+				<VictorySharedEvents events={events}>
 					<VictoryChart
 						theme={this.props.theme}
 						width={this.props.width}
@@ -211,11 +280,12 @@ class Scatter extends Component {
 							data={this.makeLegend(this.state.data)}
 							orientation="horizontal"
 							itemsPerRow={4}
-							style={{
-								labels: { fontSize: 12, fontWeight: "normal" }
-							}}
-							dataComponent={<Point y={40} />}
-							labelComponent={<VictoryLabel y={40} />}
+							dataComponent={
+								<Point style={legendDataStyle} y={40} />
+							}
+							labelComponent={
+								<VictoryLabel style={legendLabelStyle} y={40} />
+							}
 							titleComponent={
 								<VictoryLabel
 									style={[
