d59ece493b163ce36c942a4707eff412975be82d
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2018-04-18 21:35:28 -0300

    minilabel sin portal

--- src/components/mini/MiniLabel.js
+++ src/components/mini/MiniLabel.js
@@ -2,29 +2,23 @@ import React from "react";
 import { VictoryPortal } from "victory";
 
 const MiniLabel = props => (
-	<VictoryPortal>
-		<g>
-			<rect
-				x={props.x - props.width / 2}
-				y={props.y - props.height / 1.4}
-				width={props.width}
-				height={props.height}
-				rx={3}
-				ry={3}
-				style={props.style}
-				fill="#f0efeb"
-			/>
-			<text className="MiniLabel" x={props.x} y={props.y}>
-				<tspan
-					style={props.style}
-					fill={props.color}
-					text-anchor="middle"
-				>
-					{props.datum.y}%
-				</tspan>
-			</text>
-		</g>
-	</VictoryPortal>
+	<g>
+		<rect
+			x={props.x - props.width / 2}
+			y={props.y - props.height / 1.4}
+			width={props.width}
+			height={props.height}
+			rx={3}
+			ry={3}
+			style={props.style}
+			fill="#f0efeb"
+		/>
+		<text className="MiniLabel" x={props.x} y={props.y}>
+			<tspan style={props.style} fill={props.color} text-anchor="middle">
+				{props.datum.y}%
+			</tspan>
+		</text>
+	</g>
 );
 
 export default MiniLabel;
